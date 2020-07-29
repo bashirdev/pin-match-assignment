6a6c7ac063f08a839ace1af77564916745037259
bashir ahammed <bahammed16@gmail.com>
2020-07-29 08:17:36 +0400

    finally done the project

--- script.js
+++ script.js
@@ -28,40 +28,27 @@ function calculatorNum(){
 calculatorNum();
 
 function chackPin(){
-    document.getElementById('submit').addEventListener('click', function(){
+ document.getElementById('submit').addEventListener('click', function(){
+       
         if(isMatchcal.value == isMatchNumber){
      const activeDisplay= document.getElementById('notify-active');
      activeDisplay.style.display="block";
      const deactiveDisplay= document.getElementById('notify-deactive');
     deactiveDisplay.style.display="none";
+    clickFunc(false);
 } 
 if(isMatchcal.value != isMatchNumber){
     const deactiveDisplay= document.getElementById('notify-deactive');
     deactiveDisplay.style.display="block";
     const activeDisplay= document.getElementById('notify-active');
     activeDisplay.style.display="none";
-    let actionleft=[1,2,3];
-    // let checkString='sorry',
-   for (let i = 0; i <actionleft.length; i++) {
-        var actionRemaing = actionleft[i];
-        if(actionRemaing ==1){
-            document.getElementById('action-leftId').innerHTML="2 try left";
-            console.log(actionRemaing);
-        }
-        if(actionRemaing ==2){
-           document.getElementById('action-leftId').innerText="1 try left";
-           console.log(actionRemaing);
-        }if(actionRemaing==3){
-            document.getElementById('action-leftId').innerHTML="0 try left";
-            document.getElementById('submit').style.innerHTML='disabled';
-            console.log(actionRemaing);
-        }
-    }
-   
-    
-} 
+    clickFunc();
+
+  } 
 
 })
+
+
 }
 
 document.getElementById('clear').addEventListener('click', function(){
@@ -72,3 +59,31 @@ document.getElementById('clear').addEventListener('click', function(){
 document.getElementById('single-number-delete').addEventListener('click', function(){
   isMatchcal.value =  isMatchcal.value.substring(0,  isMatchcal.value.length - 1);
 })
+
+var count = 0;
+function clickFunc() {
+    count += 1;
+   var actionLeft=document.getElementById('action-leftId');
+    var click = document.getElementById('submit'); 
+   
+     click=count;
+     actionLeft =count;
+    var btn = document.getElementById('submit');
+
+
+if(click >= 15) {
+
+    btn.disabled = true;
+    btn.style.backgroundColor='red';
+   }else if(click  >= 3 ){
+    document.getElementById('action-leftId').innerHTML=2;
+   
+    } if(click >= 6){
+        document.getElementById('action-leftId').innerHTML=1; 
+    }if(click >= 12){
+        document.getElementById('action-leftId').innerHTML=0;
+    }
+}
+
+   
+
---
+++ text.js
@@ -0,0 +1,31 @@
+// let num=[1,2,3]
+// var submi= document.getElementById('submi'); 
+// let num =document.getElementById('submi').length;
+//     for (let i = 0; i < num; i++) {
+//      document.getElementById('submi')[i].addEventListener('click', function(){
+//         // var item=[1,2,3];
+//        if(num[1].length > submi){
+//         num.disabled=true;
+//         console.log(num);
+//        }
+
+// });
+       
+        
+//     }
+
+
+
+
+    // var randomNumber = Math.random();
+    // randomNumber =randomNumber * 2;
+    // randomNumber=Math.floor(randomNumber);
+    // if(randomNumber == 1){
+    //   randomNumber =  document.getElementById('action-leftId').innerHTML=2;
+    // }else if(randomNumber == 2){
+    //   randomNumber= document.getElementById('action-leftId').innerHTML= 1;
+    // }else if(randomNumber == 0){
+    //   randomNumber = document.getElementById('action-leftId').innerHTML=0;
+    // }else{
+    //     alert('sorry')
+    // }
